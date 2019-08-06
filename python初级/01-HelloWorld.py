d1c4af459fe1ac141a7afdbdced6b7c339fb9496
1Duan2Fei3 <946201032@qq.com>
2019-08-06 14:17:13 +0800

    Helloworld

--- python初级/01-HelloWorld.py
+++ python初级/01-HelloWorld.py
@@ -1,6 +1,6 @@
 # 这是第一个单行注释
 #print("hello world")
-a=21
+'''a=21
 b=10
 c=0
 c=a+b
@@ -21,6 +21,28 @@ print("6-c的值为:",c)
 a=10
 b=5
 c=a//b
-print("7-c的值为:",c)
+print("7-c的值为:",c)'''
 account='DuanFei'
 passwd=050121
+# 定义西瓜价格变量
+price = 5.5
+
+# 定义购买重量
+weight = 3.5
+
+# 计算金额
+money = price * weight
+
+print(money)
+# 定义西瓜价格变量
+price = 5.5
+
+# 定义购买重量
+weight = 3.5
+
+# 计算金额
+money = price * weight
+
+# 只要买西瓜就返 5 元
+money = money - 5
+print(money)
